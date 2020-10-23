cd865a4057f3602ec39f5e389ce7d97863c083c9
dtw <dtw@users.noreply.github.com>
2020-10-23 15:10:01 +0100

    Add comments

--- pup_get.lua
+++ pup_get.lua
@@ -44,11 +44,13 @@ function get.maneuver(name)
 	return nil
 end
 
--- Store buffs by name?
+-- Index buffs by lowercase name and store a count as value
 function get.buffs()
   local set_buff = {}
   for _, buff_id in ipairs(windower.ffxi.get_player().buffs) do
     local buff_en = res.buffs[buff_id].en:lower()
+		-- sets the value for index buff_en to the value of buff_en if it already exists OR 0, then adds 1
+		-- effectively increments existing indexes and sets new indexes to 1
     set_buff[buff_en] = (set_buff[buff_en] or 0) + 1
   end
   return set_buff
